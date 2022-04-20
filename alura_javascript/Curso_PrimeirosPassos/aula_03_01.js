d3ee9de4871c03e32229201eaa2f1a734a28c25a
Richard Borges <Richard>
2022-04-20 12:08:16 -0300

    Curso primeiros passos com JS concluido, inicio do curso de POO JS

---
+++ alura_javascript/Curso_PrimeirosPassos/aula_03_01.js
@@ -0,0 +1,8 @@
+console.log("Aula 03: Operações aritméticas");
+
+console.log(2 + 2);
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+
+console.log("Ano: " + 2022);
+console.log("2" + "2");
---
+++ alura_javascript/Curso_PrimeirosPassos/aula_03_02.js
@@ -0,0 +1,15 @@
+console.log("Aula 03: Conversão de tipos");
+
+console.log("Ano: " + 2022);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+console.log("10" / "2");
+console.log("10" * "2");
+console.log("10" - "2");
+
+console.log("7" / "2");
+console.log("Richard" / "2"); //NaN
+
+console.log(6.5);
+console.log(6,5);
