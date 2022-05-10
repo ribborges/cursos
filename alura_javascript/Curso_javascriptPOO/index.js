07f129e197238ebba18fc2017043d7889151141f
Richard Borges <Richard>
2022-05-10 12:30:41 -0300

    Módulos

--- alura_javascript/Curso_javascriptPOO/index.js
+++ alura_javascript/Curso_javascriptPOO/index.js
@@ -1,11 +1,19 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+import {Cliente} from "./Cliente.js"
+import {Conta} from "./Conta.js"
 
-const cliente2Nome = "Alice";
-const cliente2CPF = 33322211109;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+cliente1.rg = 12345678;
 
-console.log(cliente1Nome);
+const contaRicardo = new Conta;
+contaRicardo.agencia = 1001;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 33322211109;
+
+const valorSacado = contaRicardo.sacar(50);
+
+console.log(cliente1);
+console.log(cliente2);
